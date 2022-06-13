ab4f981fd02f5b91271c1136290a1593fc961c3c
rook16rus <kamalov00albert@gmail.com>
2022-06-13 13:21:54 +0300

    Теперь при скролле хедер меняет фон

--- src/js/build-in/header.js
+++ src/js/build-in/header.js
@@ -1,5 +1,17 @@
 export default function header() {
     const header = document.querySelector('.header');
+
+    window.addEventListener('scroll', () => {
+        if (header.getBoundingClientRect().top > document.documentElement.getBoundingClientRect().top) {
+            if (header.classList.contains('header--whiter')) return;
+            header.classList.add('header--white');
+        } else {
+            header.classList.remove('header--white');
+        }
+    });
+
     const intro = document.querySelector('.intro');
+    if (!intro) return;
+
     intro.style.setProperty('--header-height', header.clientHeight + 'px');
 }
