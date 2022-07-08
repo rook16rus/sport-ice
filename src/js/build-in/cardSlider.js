9d7078f89534b4647a96616d7b407c207b995ee6
Albert <kamalov00albert@gmail.com>
2022-07-08 16:47:39 +0300

    Реализовывают карточный слайдер для карты

--- src/js/build-in/cardSlider.js
+++ src/js/build-in/cardSlider.js
@@ -1,51 +1,55 @@
 export default function cardSlider() {
     jQuery(document).ready(function(){
 
-        function detect_active(){
-            // get active
-            let get_active = $(".card-slider .card-slider-slide:first-child").data("class");
-            $(".card-slider-dots button").removeClass("active");
-            $(".card-slider-dots button[data-class="+ get_active +"]").addClass("active");
-        }
+        const sliders = $('.card-slider-container');
 
-        $(".card-slider-next").click(function(){
-            $(".card-slider .card-slider-slide:first-child").hide().appendTo(".card-slider").fadeIn();
-            $.each($('.card-slider-slide'), function (index, slide) {
-                $(slide).attr('data-position', index + 1);
-            });
-            detect_active();
+        $(sliders).each((index, slider) => {
+            function detect_active(){
+                // get active
+                let get_active = $(slider).find(".card-slider-slide:first-child").data("class");
+                $(slider).find(".card-slider-dots button").removeClass("active");
+                $(slider).find(".card-slider-dots button[data-class="+ get_active +"]").addClass("active");
+            }
 
-        });
+            $(slider).find(".card-slider-next").click(function(){
+                $(slider).find(".card-slider-slide:first-child").hide().appendTo($(slider).find(".card-slider")).fadeIn();
+                $.each($(slider).find(".card-slider-slide"), function (index, slide) {
+                    $(slide).attr('data-position', index + 1);
+                });
+                detect_active();
 
-        $(".card-slider-prev").click(function(){
-            $(".card-slider .card-slider-slide:last-child").hide().prependTo(".card-slider").fadeIn();
-            $.each($('.card-slider-slide'), function (index, slide) {
-                $(slide).attr('data-position', index + 1);
             });
 
-            detect_active();
-        });
+            $(slider).find(".card-slider-prev").click(function(){
+                $(slider).find(".card-slider-slide:last-child").hide().prependTo($(slider).find(".card-slider")).fadeIn();
+                $.each($(slider).find(".card-slider-slide"), function (index, slide) {
+                    $(slide).attr('data-position', index + 1);
+                });
 
-        $(".card-slider-dots button").click(function(){
-            $(".card-slider-dots button").removeClass("active");
-            $(this).addClass("active");
-            let get_slide = $(this).attr('data-class');
-            $(".card-slider .card-slider-slide[data-class=" + get_slide + "]").hide().prependTo(".card-slider").fadeIn();
-            $.each($('.card-slider-slide'), function (index, slide) {
-                $(slide).attr('data-position', index + 1);
-            });
-        });
-
-
-        $("body").on("click", ".card-slider .card-slider-slide:not(:first-child)", function(){
-            let get_slide = $(this).attr('data-class');
-            console.log(get_slide);
-            $(".card-slider .card-slider-slide[data-class=" + get_slide + "]").hide().prependTo(".card-slider").fadeIn();
-            $.each($('.card-slider-slide'), function (index, slide) {
-                $(slide).attr('data-position', index + 1);
+                detect_active();
             });
 
-            detect_active();
-        });
+            $(slider).find(".card-slider-dots button").click(function(){
+                $(slider).find(".card-slider-dots button").removeClass("active");
+                $(this).addClass("active");
+                let get_slide = $(this).attr('data-class');
+                $(slider).find(".card-slider-slide[data-class=" + get_slide + "]").hide().prependTo($(slider).find(".card-slider")).fadeIn();
+                $.each($(slider).find(".card-slider-slide"), function (index, slide) {
+                    $(slide).attr('data-position', index + 1);
+                });
+            });
+
+
+            $("body").on("click", $(slider).find(".card-slider .card-slider-slide:not(:first-child)"), function(){
+                let get_slide = $(this).attr('data-class');
+                console.log(get_slide);
+                $(slider).find(".card-slider .card-slider-slide[data-class=" + get_slide + "]").hide().prependTo($(slider).find(".card-slider")).fadeIn();
+                $.each($(slider).find(".card-slider-slide"), function (index, slide) {
+                    $(slide).attr('data-position', index + 1);
+                });
+
+                detect_active();
+            });
+        })
     });
 }
--- src/js/build-in/mapSlider.js
+++ src/js/build-in/mapSlider.js
@@ -3,6 +3,9 @@ import {Swiper, Navigation, EffectFade, Autoplay, Pagination, HashNavigation, Gr
 Swiper.use([Navigation, EffectFade, Autoplay, Pagination, HashNavigation, Grid, FreeMode]);
 
 export default function mapSlider() {
+
+    /* Иницализация свайперов */
+
     const swiper = new Swiper('.map__slider', {
         pagination: {
             el: document.querySelector('.map__slider .swiper-pagination'),
@@ -18,11 +21,71 @@ export default function mapSlider() {
         }
     })
 
+    const citiesSwiper = new Swiper('.map__cities', {
+        slidesPerView: "auto",
+        freeMode: true,
+        centeredSlides: true
+    })
+
+    /* Смена слайда при наведении на отметку */
+
     const circles = document.querySelectorAll('.map__circle');
 
     circles.forEach((circle, index) => {
-        circle.addEventListener('mouseover', (e) => {
-            swiper.slideTo(index);
+        index += 1;
+
+        circle.addEventListener('mouseenter', (e) => {
+            $(".map .card-slider .card-slider-slide[data-class=" + index + "]").hide().prependTo(".map .card-slider").fadeIn();
+            $.each($('.map .card-slider-slide'), function (i, slide) {
+                $(slide).attr('data-position', i + 1);
+            });
         })
     })
+
+    /* Смена слайда при нажатии на город (кнопки, пагинацию) на мобилке */
+
+    const mapCities = document.querySelectorAll('.map__cities-slide');
+    const sliderDots = document.querySelectorAll('.map__slider-dots button');
+    const buttonPrev = document.querySelector('.map__slider-prev');
+    const buttonNext = document.querySelector('.map__slider-next');
+
+    mapCities.forEach((city, index) => {
+        index += 1;
+
+        city.addEventListener('click', () => {
+            $(".map .card-slider .card-slider-slide[data-class=" + index + "]").hide().prependTo(".map .card-slider").fadeIn();
+            $.each($('.card-slider-slide'), function (i, slide) {
+                $(slide).attr('data-position', i + 1);
+            });
+
+            citiesSwiper.slideTo(index - 1);
+            citiesSwiper.slides.forEach((slide, index) => {
+                (index === citiesSwiper.activeIndex) ? slide.classList.add('active') : slide.classList.remove('active');
+            });
+        })
+    })
+
+    sliderDots.forEach((dot, index) => {
+        dot.addEventListener('click', () => {
+            citiesSwiper.slideTo(index);
+            citiesSwiper.slides.forEach((slide, index) => {
+                (index === citiesSwiper.activeIndex) ? slide.classList.add('active') : slide.classList.remove('active');
+            });
+        })
+    })
+
+    buttonPrev.addEventListener('click', () => {
+        (citiesSwiper.activeIndex === 0) ? citiesSwiper.slideTo(citiesSwiper.activeIndex - 1) : citiesSwiper.slidePrev();
+        citiesSwiper.slides.forEach((slide, index) => {
+            (index === citiesSwiper.activeIndex) ? slide.classList.add('active') : slide.classList.remove('active');
+        });
+    });
+
+    buttonNext.addEventListener('click', () => {
+        (citiesSwiper.activeIndex === citiesSwiper.slides.length - 1) ? citiesSwiper.slideTo(0) : citiesSwiper.slideNext();
+        citiesSwiper.slides.forEach((slide, index) => {
+            (index === citiesSwiper.activeIndex) ? slide.classList.add('active') : slide.classList.remove('active');
+        });
+    });
 }
+
