ceefd3010cb6e2426ec5aafc3cc1cda529bd16e6
Albert <kamalov00albert@gmail.com>
2022-07-13 14:09:15 +0300

    Реализовал появление модалки успеха

--- src/assets/backend/backend.js
+++ src/assets/backend/backend.js
@@ -1,3 +1,43 @@
 document.addEventListener('DOMContentLoaded', () => {
-    
+    handleForm();
 });
+
+function handleForm() {
+    const forms = Array.from(document.querySelectorAll('.js-handle-form'));
+    forms.forEach(form => {
+        const url = form.action;
+        const submitBtn = form.querySelector('.js-submit-form');
+
+        form.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            submitBtn.classList.add('disabled');
+
+            const formData = new FormData(form);
+
+            fetch(url, {
+                body: formData,
+                method: "POST"
+            })
+                .then(response => {
+                    if (response.ok) {
+                        window.openModal("success");
+                        form.reset();
+                    } else {
+                        window.openModal("success");
+                    }
+
+                    return response.json();
+                })
+                .then(data => {
+                    console.log(data);
+                })
+                .catch((err) => {
+                    console.error(err);
+                })
+                .finally(() => {
+                    submitBtn.classList.remove('disabled');
+                })
+        })
+    })
+}
--- src/js/build-in/error.js
+++ src/js/build-in/error.js
@@ -1,6 +1,8 @@
 export default function error() {
     const header = document.querySelector('.header');
     const error = document.querySelector('.error')
+    if (!error) return;
 
     error.style.setProperty('--header-height', header.clientHeight + 'px');
 }
+
