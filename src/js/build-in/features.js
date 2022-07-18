f7e0dc184469f6a4ed56fe768ccef32fd320eb28
Albert <kamalov00albert@gmail.com>
2022-07-18 13:26:04 +0300

    Правки

--- src/js/build-in/features.js
+++ src/js/build-in/features.js
@@ -1,19 +1,41 @@
+import gsap from 'gsap';
+import {ScrollTrigger} from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
 export default function features() {
-    const titles = document.querySelectorAll('.features__item-title');
+    const timeline = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".features .container",
+            scrub: true,
+            pin: ".features .container",
+            start: "top 20%",
+        }
+    });
 
-    window.addEventListener('scroll', (e) => {
-        titles.forEach(title => {
-            const positionTop = title.getBoundingClientRect().top;
-            const containerHeight = title.closest('.features__item').clientHeight;
-            const windowHeight = document.documentElement.clientHeight;
-            const startTrigger = windowHeight / 100 * 75;
-            const endTrigger = startTrigger - containerHeight;
 
-            if (positionTop < startTrigger && positionTop > endTrigger) {
-                title.classList.add('features__item-title--red');
-            } else {
-                title.classList.remove('features__item-title--red')
-            }
+    gsap.utils.toArray(".features__item:not(:first-child)").forEach(item => {
+        gsap.set(item, {
+            yPercent: 100,
+            opacity: 0
         })
     });
+
+    timeline
+        .to(".features__item:not(:first-child)", {
+            yPercent: 0,
+            stagger: 4,
+            opacity: 1,
+            duration: 2,
+            ease: "none",
+        })
+        .to(".features__item:first-child", {
+            opacity: 0,
+            duration: 2
+        }, "-=2")
+        .to(".features__item:nth-child(2)", {
+            opacity: 0,
+            duration: 2,
+        })
+
 }
