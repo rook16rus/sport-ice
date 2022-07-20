ab9ad24e9bee46756ebb7e69681cfae34d7fe802
rook16rus <kamalov00albert@gmail.com>
2022-07-20 15:48:33 +0300

    Updates

--- .prettierrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    singleQuote: true,
-    semi: true,
-    printWidth: 150,
-    tabWidth: 4,
-};
--- src/js/build-in/error.js
+++
@@ -1,8 +0,0 @@
-export default function error() {
-    const header = document.querySelector('.header');
-    const error = document.querySelector('.error')
-    if (!error) return;
-
-    error.style.setProperty('--header-height', header.clientHeight + 'px');
-}
-
